7128707f3796a2d6667036a5d2126c3af16d9296
Mohammad Azmi <mohammad.azmi.id@gmail.com>
2026-02-07 23:03:54 +0700

    css hot reload

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 dev:
-	meson setup build --wipe --prefix $(shell pwd)/result
+	meson setup build -Ddev=true --wipe --prefix $(shell pwd)/result
 	meson install -C build
+
+	@trap 'kill 0' INT; \
+	esbuild --bundle src/style.css --outfile=build/src/style.css --watch=forever & \
 	./result/bin/simple-bar
 
 gen-types:
--- src/App.ts
+++ src/App.ts
@@ -1,65 +1,103 @@
-import GObject from "gi://GObject"
-import Gio from "gi://Gio"
-import GLib from "gi://GLib"
-import Gtk from "gi://Gtk?version=4.0"
-import Gdk from "gi://Gdk?version=4.0"
-import Astal from "gi://Astal?version=4.0"
-import Bar from "./bar/Bar"
+import GObject from 'gi://GObject';
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+import Gtk from 'gi://Gtk?version=4.0';
+import Gdk from 'gi://Gdk?version=4.0';
+import Astal from 'gi://Astal?version=4.0';
+import Bar from './bar/Bar';
+
+const defaultOptions = {
+    cssPath: 'build/src/style.css',
+};
+
+type AppOptions = typeof defaultOptions;
 
 export default class App extends Astal.Application {
     static {
-        GObject.registerClass(this)
+        GObject.registerClass(this);
     }
 
-    static instance: App
-    private bar!: Bar
+    static instance: App;
 
-    constructor() {
+    readonly options: AppOptions;
+
+    private bar!: Bar;
+    private monitor!: Gio.FileMonitor;
+
+    constructor(options: Partial<AppOptions> = {}) {
         super({
-            applicationId: "my.awesome.simple-bar",
+            applicationId: 'enoughbar',
             flags: Gio.ApplicationFlags.HANDLES_COMMAND_LINE,
-        })
+        });
+
+        this.options = {
+            ...defaultOptions,
+            ...options,
+        };
     }
 
     private initCss() {
-        const provider = new Gtk.CssProvider()
-        provider.load_from_resource("/style.css")
+        const provider = new Gtk.CssProvider();
+        const file = Gio.File.new_for_path(this.options.cssPath);
+        this.monitor = file.monitor(Gio.FileMonitorFlags.WATCH_MOVES, null);
+
+        if (file.query_exists(null)) {
+            provider.load_from_path(this.options.cssPath);
+        } else {
+            console.log(`${this.options.cssPath} does not exist`);
+        }
 
         Gtk.StyleContext.add_provider_for_display(
             Gdk.Display.get_default()!,
             provider,
-            Gtk.STYLE_PROVIDER_PRIORITY_USER,
-        )
+            Gtk.STYLE_PROVIDER_PRIORITY_USER
+        );
+
+        this.monitor.connect(
+            'changed',
+            (_fileMonitor, file, otherFile, eventType) => {
+                switch (eventType) {
+                    case Gio.FileMonitorEvent.CHANGED:
+                        provider.load_from_path(this.options.cssPath);
+                        break;
+
+                    case Gio.FileMonitorEvent.DELETED:
+                        provider.load_from_string('');
+                        break;
+
+                    case Gio.FileMonitorEvent.CREATED:
+                        provider.load_from_path(this.options.cssPath);
+                        break;
+                }
+            }
+        );
     }
 
     vfunc_command_line(command_line: Gio.ApplicationCommandLine): number {
-        const argv = command_line.get_arguments()
+        const argv = command_line.get_arguments();
 
         if (command_line.isRemote) {
-            // app is already running we can print to remote
-            command_line.print_literal("hello from the main instance\n")
-
-            // for example, we could toggle the visibility of the bar
-            if (argv.length >= 2 && argv[0] == "toggle" && argv[1] == "bar") {
-                this.bar.visible = !this.bar.visible
+            // we could toggle the visibility of the bar
+            if (argv.length >= 1 && argv[0] == 'toggle') {
+                this.bar.visible = !this.bar.visible;
             }
 
             // exit remote process
-            command_line.done()
+            command_line.done();
         } else {
             // main instance, initialize stuff here
-            this.initCss()
-            this.bar = new Bar()
-            this.add_window(this.bar)
+            this.initCss();
+            this.bar = new Bar();
+            this.add_window(this.bar);
         }
 
-        return 0
+        return 0;
     }
 
     // entry point of our app
     static async main(argv: string[]): Promise<number> {
-        App.instance = new App()
-        GLib.set_prgname("simple-bar")
-        return App.instance.runAsync(argv)
+        App.instance = new App();
+        GLib.set_prgname('enoughbar');
+        return App.instance.runAsync(argv);
     }
 }
